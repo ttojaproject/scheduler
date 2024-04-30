0f070184a7682128b466c27246b1eee79a76193b
hyoungjooKim <kim167429@gmail.com>
2024-04-30 14:31:57 +0900

    [기능] 프로젝트 신규 생성 및 오류 해결 [본문] - gradle-8.7버전 오류로 인해 8.5버전 변경 후 정상 작동 [해결한 이슈] - Could not move temporary workspace [참고 이슈] - gradle-8.7버전 오류 링크 : https://velog.io/@jyl9311/IntelliJ-Could-not-move-temporary-workspace-%EC%98%A4%EB%A5%98-%ED%95%B4%EA%B2%B0

---
+++ src/test/java/ttoja/project/scheduler/SchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package ttoja.project.scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
